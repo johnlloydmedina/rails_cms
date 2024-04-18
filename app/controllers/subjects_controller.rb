1db70887987734e57753409eaf21da28c4dbdf64
JL <johnlloydmedina.dev@gmail.com>
2024-04-18 16:23:32 +0800

    Subject forms

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,24 @@
+class SubjectsController < ApplicationController
+  
+  layout false
+
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new()
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  
+end
