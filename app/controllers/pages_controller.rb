fb2d3c9f36726a1380a41df7c8a9386c72707370
JL <johnlloydmedina.dev@gmail.com>
2024-04-24 08:40:33 +0800

    nest page to subject

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,11 @@ class PagesController < ApplicationController
     layout "admin"
 
     before_action :confirm_logged_in
+    before_action :find_subject
     
       def index
-        @pages = Page.sorted
+        # @pages = Page.where(::subject_id => @subject.id).sorted
+        @pages = @subject.pages.sorted
       end
     
       def show
@@ -13,7 +15,7 @@ class PagesController < ApplicationController
       end
 
       def new
-        @page = Page.new()
+        @page = Page.new({:subject_id => @subject_id, :name => "Default"})
         @subjects = Subject.order('position ASC')
         @page_count = Page.count + 1
       end
@@ -22,7 +24,7 @@ class PagesController < ApplicationController
         @page = Page.new(page_params)
         if @page.save
             flash[:notice] = "Page created successfully"
-            redirect_to(:action => 'index')
+            redirect_to(:action => 'index', :subject_id => @subject.id)
         else
             @subjects = Subject.order('position ASC')
             @page_count = Page.count + 1
@@ -34,7 +36,7 @@ class PagesController < ApplicationController
         @page = Page.find(params[:id])
         if @page.update_attributes(page_params)
             flash[:notice] = "Page updated successfully"
-            redirect_to(:action => 'show', :id => @page.id)
+            redirect_to(:action => 'show', :id => @page.id, :subject_id => @subject.id)
         else
             @subjects = Subject.order('position ASC')
             @page_count = Page.count
@@ -55,12 +57,18 @@ class PagesController < ApplicationController
       def destroy
         page = Page.find(params[:id]).destroy
         flash[:notice] = "Page #{page.name} deleted successfully."
-        redirect_to(:action => 'index')
+        redirect_to(:action => 'index', :subject_id => @subject.id)
       end
 
       private
 
       def page_params
         params.require(:page).permit(:name, :subject_id, :permalink, :position, :visible)
+      end
+
+      def find_subject
+        if params[:subject_id]
+          @subject = Subject.find(params[:subject_id])
+        end
       end
 end
