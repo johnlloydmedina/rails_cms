fb79b0f7b75d954350fb7587461671e81a7f5160
JL <johnlloydmedina.dev@gmail.com>
2024-04-24 11:24:03 +0800

    implement callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # get 'demo/index'
   # root :to => "demo#index"
-  root "demo#index"
+  root "public#index"
 
+  get 'show/:permalink', :to => 'public#show'
   get 'admin', :to => "access#index"
 
   match ':controller(/:action(/:id))', :via => [:get, :post]
